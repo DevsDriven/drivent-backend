7e44fe3ddb647d45d78ad83d377432380e5bb941
carloshnp <carloshnpwork@gmail.com>
2023-03-16 00:26:03 -0300

    feat: activities controller

---
+++ src/controllers/activities-controller.ts
@@ -0,0 +1,24 @@
+import { AuthenticatedRequest } from "@/middlewares";
+import { Response } from "express";
+import httpStatus from "http-status";
+
+export async function getActivitiesDay(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+  try {
+    const days = "await activitiesService.getDay(Number(userId))";
+    return res.status(httpStatus.OK).send(days);
+  } catch (error) {
+    return res.sendStatus(httpStatus.BAD_REQUEST);  
+  }
+}
+
+export async function getActivitiesFromDate(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+  const { dayId } = req.params;
+  try {
+    const activities = "await activitiesService.getActivities(Number(userId), Number(dayId))";
+    return res.status(httpStatus.OK).send(activities);
+  } catch (error) {
+    return res.sendStatus(httpStatus.BAD_REQUEST);
+  }
+}
